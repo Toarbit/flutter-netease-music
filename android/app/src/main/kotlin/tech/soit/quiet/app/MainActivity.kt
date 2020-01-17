ffe4bb07808863d1865b29d286e495688f75d450
shor <8676668+Toarbit@users.noreply.github.com>
2020-01-17 22:37:55 +0800

    实现 Android Flutter Plugin v2

--- android/app/src/main/kotlin/tech/soit/quiet/app/MainActivity.kt
+++ android/app/src/main/kotlin/tech/soit/quiet/app/MainActivity.kt
@@ -1,9 +1,7 @@
 package tech.soit.quiet.app
 
 import android.content.Intent
-import android.os.Bundle
-import io.flutter.app.FlutterActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity() {
 
@@ -17,10 +15,10 @@ class MainActivity : FlutterActivity() {
     }
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        GeneratedPluginRegistrant.registerWith(this)
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        GeneratedPluginRegistrant.registerWith(this)
+//    }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
@@ -30,7 +28,8 @@ class MainActivity : FlutterActivity() {
     private fun route(intent: Intent) {
         when (intent.getStringExtra(KEY_DESTINATION)) {
             DESTINATION_PLAYING_PAGE -> {
-                flutterView.pushRoute("/playing")
+//                flutterView.pushRoute("/playing")
+                this.flutterEngine?.navigationChannel?.pushRoute("/playing")
             }
         }
     }
